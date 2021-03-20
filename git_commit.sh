01edb7b7021435735039092e732d86bba4fb6497
Sundeep Chadha <sundeep_chadha@yahoo.com>
2021-03-20 14:25:30 -0500

    More updates

---
+++ git_commit.sh
@@ -0,0 +1,14 @@
+echo ""
+echo "Showing the current status"
+git status
+sleep 2
+echo ""
+echo ""
+echo "=====> Now starting the commit process <====="
+echo ""
+
+
+
+git commit -m "More updates" Linear/Charts_With_Numbers/*jpg
+git commit -m "More updates" Linear/Charts_With_Numbers/*jpg
+
